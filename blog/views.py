e2e3297e2691ad01e9d1b94323fbc06423bda2b5
hexinze <hexinze@ajmide.com>
2017-09-19 10:31:54 +0800

    ok add query set

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from .models import Post
+from django.utils import timezone
 # Create your views here.
 def post_list(request):
-	return render(request, 'blog/post_list.html', {})
- 
+    posts = Post.objects.order_by('created_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+ # def post_detail(request, pk):
+ #    post = get_object_or_404(Post, pk=pk)
+ #    return render(request, 'blog/post_detail.html', {'post': post})
