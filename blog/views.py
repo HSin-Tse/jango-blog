a0eb99c31cbdf082202448c4f7213a913d36782d
hexinze <hexinze@ajmide.com>
2017-09-22 14:31:38 +0800

    ok go

--- blog/views.py
+++ blog/views.py
@@ -10,6 +10,5 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
-def post_log(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/log.html', {'post': post})    
+def post_log(request):
+    return render(request, 'blog/log.html')    
